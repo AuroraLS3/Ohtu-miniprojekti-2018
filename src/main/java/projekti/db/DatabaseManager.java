494765cae8a3ae5ccc27a95779013cb3a008ae8d
Vili Lipo <vili.lipo@helsinki.fi>
2018-11-19 15:37:33 +0200

    requested fixes

--- src/main/java/projekti/db/DatabaseManager.java
+++ src/main/java/projekti/db/DatabaseManager.java
@@ -2,6 +2,7 @@ package projekti.db;
 
 import java.sql.Statement;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -10,6 +11,7 @@ import java.util.stream.Collectors;
 
 /**
  * Is used to manage database configuration.
+ * 
  * @author vili
  *
  */
@@ -19,19 +21,20 @@ class DatabaseManager {
 	private String password;
 	private Connection conn;
 	private final String schemaPath = "sql/schema.sql";
-	
+
 	/**
 	 * Creates database if it does not exist.
-	 * @param url  url for driver
-	 * @param username  username for db  "sa"
-	 * @param password  password for db ""
-	 * @throws SQLException 
+	 * 
+	 * @param url      url for driver
+	 * @param username username for db "sa"
+	 * @param password password for db ""
+	 * @throws SQLException
 	 */
 	public DatabaseManager(String url, String username, String password) throws SQLException {
 		try {
 			Class.forName("org.h2.Driver");
 		} catch (ClassNotFoundException e) {
-			System.out.println("Can't find H2 driver");
+			throw new IllegalStateException("Cant find H2 driver");
 		}
 		this.url = url;
 		this.username = username;
@@ -39,8 +42,10 @@ class DatabaseManager {
 		this.conn = null;
 		this.setupSchema();
 	}
+
 	/**
 	 * Make a safe connection. Useful if autocommit is of.
+	 * 
 	 * @return Connection.
 	 * @throws SQLException
 	 */
@@ -57,22 +62,30 @@ class DatabaseManager {
 
 	/**
 	 * Schema is written in "IF EXISTS" format so this is safe to call every time.
-	 * 
+	 *
 	 * @throws SQLException if something goes wrong.
 	 */
 	public void setupSchema() throws SQLException {
-		this.connect();
-		String schema = readResourceFile(this.schemaPath);
-		Statement stmnt = conn.createStatement();
-		stmnt.executeUpdate(schema);
-		conn.close();
+		try (Connection c = this.connect();
+				Statement stmnt = c.createStatement()){
+			this.connect();
+			String schema = readResourceFile(this.schemaPath);
+			stmnt.executeUpdate(schema);
+		} catch (SQLException e) {
+			throw e;
+		}
+
 	}
 
 	private String readResourceFile(String filename) {
-		InputStreamReader isr = new InputStreamReader(ClassLoader.getSystemClassLoader().getResourceAsStream(filename));
-		BufferedReader br = new BufferedReader(isr);
-		String value = br.lines().collect(Collectors.joining("\n"));
-		return value;
+		try (InputStreamReader isr = new InputStreamReader(
+				ClassLoader.getSystemClassLoader().getResourceAsStream(filename));
+				BufferedReader br = new BufferedReader(isr)) {
+			String value = br.lines().collect(Collectors.joining("\n"));
+			return value;
+		} catch (IOException ex) {
+			return "";
+		}
 	}
 
 }
