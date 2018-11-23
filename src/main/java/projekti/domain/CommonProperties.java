c766500a85d153a868ea344a1ddbc437774fa7e9
Risto Lahtela <djrapitops@hotmail.com>
2018-11-23 15:28:43 +0200

    Added new Property class (#9)
    Property class allows more flexible addition of different properties for suggestions in the future.

---
+++ src/main/java/projekti/domain/CommonProperties.java
@@ -0,0 +1,12 @@
+package projekti.domain;
+
+/**
+ * Contains common properties that might be used in multiple different objects.
+ *
+ * @author Rsl1122
+ */
+public class CommonProperties {
+
+    static final Property<Integer> ID = new Property<>("ID", Integer.class);
+
+}
