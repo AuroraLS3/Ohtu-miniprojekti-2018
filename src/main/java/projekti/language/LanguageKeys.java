9970e12cb24d77202d8dae0dba0970678cea4566
Risto Lahtela <djrapitops@hotmail.com>
2018-12-07 17:00:44 +0200

    Language keys (#34)
    * language enum

---
+++ src/main/java/projekti/language/LanguageKeys.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projekti.language;
+
+/**
+ *
+ * @author vililipo
+ */
+public enum LanguageKeys {
+    GREET("greetMessage"),
+    MAINCOMMANDS("mainCommands"),
+    COMMAND("command"),
+    QUIT("exitMessage"),
+    NONSUP("nonSupportedCommand"),
+    NOREC("noRecommendationFound"),
+    TYPELIST("typeListings"),
+    RECADDED("recommendationAdded"),
+    RECNOTADDED("recommendationNotAdded"),
+    SELECTIDQUERY("selectIdQuery"),
+    NONVALIDID("nonValidID"),
+    CONFIRMFAIL("invalidInput"),
+    DELETECONFIRM("deleteConfirmation"),
+    DELSUCCESS("deleteSuccess"),
+    DELCANCEL("deleteCancellation"),
+    ENTERNEW("enterNew"),
+    ORLEAVE("orLeave"),
+    NOTUP("notUpdated"),
+    UPDATECONFIR("updateConfirmation"),
+    
+    ;
+    
+    private final String key;
+    
+    
+    private LanguageKeys(String key) {
+        this.key = key;
+    }
+    
+}
