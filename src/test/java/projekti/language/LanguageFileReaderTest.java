95420b570a1bd2d0f6b24defb7dbf948e78b0361
Lipo Vili S <vililipo@da5-cs-b221-04.pc.helsinki.fi>
2018-12-10 14:48:10 +0200

    requested changes to test file

--- src/test/java/projekti/language/LanguageFileReaderTest.java
+++ src/test/java/projekti/language/LanguageFileReaderTest.java
@@ -12,10 +12,10 @@ public class LanguageFileReaderTest {
 	public void readLanguageTest() {
 		LanguageFileReader lr = new LanguageFileReader();	
 		Locale locale = Locale.createWith(new LanguageFileReader(), LanguageFiles.ENGLISH.getResourcePath());
-		assertTrue("Welcome to the reading recommendation app !".contentEquals(locale.get(LanguageKeys.GREET)));
-		assertTrue("Supported commands:".contentEquals(locale.getAsList(LanguageKeys.MAINCOMMANDS).get(0)));
+		assertEquals("Welcome to the reading recommendation app !", locale.get(LanguageKeys.GREET));
+		assertEquals("Supported commands:", locale.getAsList(LanguageKeys.MAINCOMMANDS).get(0));
 		Locale suomi = Locale.createWith(new LanguageFileReader(), LanguageFiles.SUOMI.getResourcePath());
-		assertTrue("Tervetuloa lukuvinkki applikaatioon!".contentEquals(suomi.get(LanguageKeys.GREET)));
+		assertEquals("Tervetuloa lukuvinkki applikaatioon!", suomi.get(LanguageKeys.GREET));
 		assertEquals("Tervetuloa lukuvinkki applikaatioon!", (suomi.getAsList(LanguageKeys.GREET).get(0)));
 		assertEquals("Valitse vinkin tyyppi\nvinkin tyyppi (vaihtoehdot: book, blog, other): \n", suomi.get(LanguageKeys.TYPELIST));
 	}
