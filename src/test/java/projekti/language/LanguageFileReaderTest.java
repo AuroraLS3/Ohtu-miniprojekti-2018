700aa218639c04fd02a0313b661307fab378fc56
Lipo Vili S <vililipo@da5-cs-b221-04.pc.helsinki.fi>
2018-12-10 14:56:03 +0200

    test fixes

--- src/test/java/projekti/language/LanguageFileReaderTest.java
+++ src/test/java/projekti/language/LanguageFileReaderTest.java
@@ -4,20 +4,48 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import org.junit.Before;
 
 import org.junit.Test;
 
 public class LanguageFileReaderTest {
-	@Test
-	public void readLanguageTest() {
-		LanguageFileReader lr = new LanguageFileReader();	
-		Locale locale = Locale.createWith(new LanguageFileReader(), LanguageFiles.ENGLISH.getResourcePath());
-		assertEquals("Welcome to the reading recommendation app !", locale.get(LanguageKeys.GREET));
-		assertEquals("Supported commands:", locale.getAsList(LanguageKeys.MAINCOMMANDS).get(0));
-		Locale suomi = Locale.createWith(new LanguageFileReader(), LanguageFiles.SUOMI.getResourcePath());
-		assertEquals("Tervetuloa lukuvinkki applikaatioon!", suomi.get(LanguageKeys.GREET));
-		assertEquals("Tervetuloa lukuvinkki applikaatioon!", (suomi.getAsList(LanguageKeys.GREET).get(0)));
-		assertEquals("Valitse vinkin tyyppi\nvinkin tyyppi (vaihtoehdot: book, blog, other): \n", suomi.get(LanguageKeys.TYPELIST));
-	}
+
+    private LanguageFileReader lfr;
+    private Locale english;
+    private Locale suomi;
+
+    @Before
+    public void beforeTest() {
+        this.lfr = new LanguageFileReader();
+        english = Locale.createWith(lfr, LanguageFiles.ENGLISH.getResourcePath());
+        suomi = Locale.createWith(lfr, LanguageFiles.SUOMI.getResourcePath());
+    }
+
+    @Test
+    public void normalUseCaseEnglishTest() {
+        assertEquals("Welcome to the reading recommendation app !", english.get(LanguageKeys.GREET));
+
+    }
+
+    @Test
+    public void normalUseCaseSuomiTest() {
+        assertEquals("Tervetuloa lukuvinkki applikaatioon!", suomi.get(LanguageKeys.GREET));
+    }
+
+    @Test
+    public void asAListEnglishTest() {
+        assertEquals("Supported commands:", english.getAsList(LanguageKeys.MAINCOMMANDS).get(0));
+
+    }
+
+    @Test
+    public void asListSuomiTest() {
+        assertEquals("Tervetuloa lukuvinkki applikaatioon!", (suomi.getAsList(LanguageKeys.GREET).get(0)));
+    }
+
+    @Test
+    public void listAsAStringSuomiTest() {
+        assertEquals("Valitse vinkin tyyppi\nvinkin tyyppi (vaihtoehdot: book, blog, other): \n", suomi.get(LanguageKeys.TYPELIST));
+    }
 
 }
