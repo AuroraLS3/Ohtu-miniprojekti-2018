1d4c61a7f573dc1fd77299810be64115014524de
Vili Lipo <vili.lipo@helsinki.fi>
2018-12-10 11:26:23 +0200

    test lists too

--- src/test/java/projekti/language/LanguageFileReaderTest.java
+++ src/test/java/projekti/language/LanguageFileReaderTest.java
@@ -13,8 +13,10 @@ public class LanguageFileReaderTest {
 		ArrayList<Locale> locales = lr.readLanguages("json/lang.json");
 		Locale locale = locales.get(0);
 		assertTrue("Welcome to the reading recommendation app !".contentEquals(locale.get(LanguageKeys.GREET)));
+		assertTrue("Supported commands:".contentEquals(locale.getAsList(LanguageKeys.MAINCOMMANDS).get(0)));
 		Locale suomi = locales.get(1);
 		assertTrue("Tervetuloa lukuvinkki applikaatioon!".contentEquals(suomi.get(LanguageKeys.GREET)));
+		
 		
 	}
 
