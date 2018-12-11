32ae50de1662bd46df2e42b329899ad77f296bc3
nigosH <32242577+nigoshh@users.noreply.github.com>
2018-12-11 18:05:34 +0200

    removed extra getAllRecommendations method

--- src/main/java/projekti/UI/commands/ListAll.java
+++ src/main/java/projekti/UI/commands/ListAll.java
@@ -8,7 +8,7 @@ import projekti.domain.Recommendation;
 public class ListAll implements Command {
     private RecHelper rh;
     private DBHelper db;
-    
+
     public ListAll(RecHelper rh, DBHelper db) {
         this.rh = rh;
         this.db = db;
@@ -18,20 +18,12 @@ public class ListAll implements Command {
     public void execute() throws SQLException {
         listRecommendations();
     }
-    
+
     private void listRecommendations() throws SQLException {
-        List<Recommendation> recommendations = getAllRecommendations();
-        rh.updateIDList(); 
+        List<Recommendation> recommendations = rh.getAllRecommendations();
+        rh.updateIDList(recommendations);
         for (int i = 0; i < recommendations.size(); i++) {
             rh.getIO().println(i + recommendations.get(i).toString());
         }
-    }
-
-    private List<Recommendation> getAllRecommendations() throws SQLException {
-        List<Recommendation> recommendations = new ArrayList<>();
-        recommendations.addAll(db.getBookDAO().findAll());
-        recommendations.addAll(db.getBlogDAO().findAll());
-        recommendations.addAll(db.getOtherDAO().findAll());
-        return recommendations;
     }
 }
